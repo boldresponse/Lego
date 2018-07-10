68aae625777ee794b544a83a2005427c17f05fa5
boldresponse <edoneil@gmail.com>
2018-07-09 20:29:05 -0400

    Get rid of jupyter line refs

--- Duplo.py
+++ Duplo.py
@@ -14,40 +14,24 @@ import pandas as pd
 sets=pd.Series(['10532-1','10590-1','10583-1','10567-1','10615-1','10528-1','10600-1','10570-1','10819-1','10617-1','10599-1','10818-1','10577-1','10813-1','10808-1','10580-1','10811-1','4962-1'],name='sets')
 
 
-# In[3]:
-
 
 df=pd.read_csv('/Users/ed/Downloads/lego-database/sets.csv') #Point this to where you saved the database
 
 
-# In[4]:
-
-
 df.head()
 
-
-# In[5]:
-
-
+#Input set list
 sets=pd.Series(['10532-1','10590-1','10583-1','10567-1','10615-1','10528-1','10600-1','10570-1','10819-1','10617-1','10599-1','10818-1','10577-1','10813-1','10808-1','10580-1','10811-1','4962-1'],name='sets')
 Mysets=df[df.set_num.isin(sets)]
 Mysets
 
-
-# In[6]:
-
-
+#pull part totals from database
 Mysets.num_parts.sum()
 
 
-# In[7]:
 
 
-#Mysets.price[Mysets.set_num[sets[0]]=1
-
-
-# In[8]:
-
+#Scrape price data
 
 from bs4 import BeautifulSoup
 import numpy as np
@@ -66,35 +50,25 @@ for set in sets:
         #print(set+ " " + pric)
 
 
-# In[9]:
-
-
 Mysets['price2'] = Mysets['price2'].apply(pd.to_numeric)
-
-
-# In[10]:
 
 
 Mysets.price2.sum()
 
-
-# In[11]:
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[13]:
-
-
+#plot
 plt.scatter(Mysets.num_parts,Mysets.price2,)
 plt.xlabel("Pieces")
 plt.ylabel("USD")
 plt.title("Duplo Price Per Piece")
 
 
-# In[ ]:
+
 
 
 
